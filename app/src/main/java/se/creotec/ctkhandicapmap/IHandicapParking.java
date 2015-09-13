5573f2db589ecee3d4bc985aeac0a2d81c416cda
Alexander Håkansson <al3xand3r.h@gmail.com>
2015-09-13 16:20:34 +0200

    Creates an interface for parking spots

---
+++ app/src/main/java/se/creotec/ctkhandicapmap/IHandicapParking.java
@@ -0,0 +1,14 @@
+package se.creotec.ctkhandicapmap;
+
+/**
+ * @author Alexander Håkansson
+ * @since 2015-09-13
+ */
+public interface IHandicapParking {
+    String getID();
+    String getName();
+    double getLatitude();
+    double getLongitude();
+    long getTotalParkingCount();
+    String getMaxParkingTime();
+}
